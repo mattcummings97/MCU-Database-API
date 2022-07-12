992b9bdefe01de296a3370686765783d2bc15051
Matthew Cummings <cummingsmatthew97@gmail.com>
2022-07-12 15:16:50 -0400

    added heroku port

--- server.js
+++ server.js
@@ -109,6 +109,6 @@ app.get('/api/:marvelUniverseDataBase',(request,response)=>{
 })
 
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is now running on port ${PORT}! Avengers.... Assemble!`)
 })
